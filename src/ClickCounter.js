e7bd2f24b69227de0e3ab6b869dce0331abc6493
aakashrjrajpoot12 <aakashrjrajpoot@gmail.com>
2023-02-17 17:35:05 +0530

    1 file modified

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import IncrementAge from './IncrementAge';
-class Clickcounter extends React.Component{
-  render(){
-    return(
+class Clickcounter extends React.Component {
+  render() {
+    return (
       <React.Fragment>
-        <button onclick={()=> this.props.increaseAge()}>
+        <button onclick={() => {this.props.increaseAge()}}>
           My Age {this.props.myAge}
-          </button>
-        </React.Fragment>
-        //React Fragment is a feature in React that allows you to return multiple elements from a React component by allowing you to group a list of children without adding extra nodes to the DOM.
-        // React Fragments is faster and more space-efficient as compared to an extra div (no need to create an extra DOM node). 
+        </button>
+      </React.Fragment>
+      //React Fragment is a feature in React that allows you to return multiple elements from a React component by allowing you to group a list of children without adding extra nodes to the DOM.
+      // React Fragments is faster and more space-efficient as compared to an extra div (no need to create an extra DOM node).
     );
   }
 }
-export default IncrementAge(Clickcounter,5);
+export default IncrementAge(Clickcounter, 5);
