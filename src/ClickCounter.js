4bc40ce726252017b42c58c2bd78e2356d172d7b
aakashrjrajpoot12 <aakashrjrajpoot@gmail.com>
2023-02-17 14:47:43 +0530

    1 file modified

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -8,6 +8,7 @@ class Clickcounter extends React.Component{
           My Age {this.props.myAge}
           </button>
         </React.Fragment>
+        //React Fragment is a feature in React that allows you to return multiple elements from a React component by allowing you to group a list of children without adding extra nodes to the DOM.
     );
   }
 }
